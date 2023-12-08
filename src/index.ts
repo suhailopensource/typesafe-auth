feb8c50fde91b87a159f60ca8a337cf7e833768d
Suhail <suhailcollegemail@gmail.com>
2023-12-08 22:02:39 +0530

    typesafe authentication

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import router from './router'
+
+const app = express()
+
+app.use(cors({
+    credentials:true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+
+const server = http.createServer(app)
+
+server.listen(8080,()=>{
+    console.log(`server is running at http://localhost:8080/`)
+})
+
+const MONGO_URL = "< i am not dumb to give my url lol :) >"
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+mongoose.connection.on('error',(error:Error)=>{
+    console.log(error)
+})
+
+app.use('/',router())
---
+++ src/router/users.ts
@@ -0,0 +1,10 @@
+import { isAuthenticated, isOwner } from '../middlewares/index';
+import { deleteUser, getAllUsers, updateUser } from './../controllers/users';
+import express from 'express'
+
+
+export default(router: express.Router)=>{
+   router.get('/users',isAuthenticated,getAllUsers)
+   router.delete('/users/:id',isAuthenticated,isOwner,deleteUser)
+   router.put('/users/:id',isAuthenticated,isOwner,updateUser)
+} 
